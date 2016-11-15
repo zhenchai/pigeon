3784721f6a8f0b57106d6d77f0c47e2cfe7a47ce
ChenChongze <chenchongze@ChenChongzedeMacBook-Pro.local>
2016-11-15 11:21:58 +0800

    fix and improve

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/config/spring/ServiceInitializeListener.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/provider/config/spring/ServiceInitializeListener.java
@@ -1,6 +1,7 @@
 package com.dianping.pigeon.remoting.provider.config.spring;
 
 import com.dianping.pigeon.log.Logger;
+import com.dianping.pigeon.util.CollectionUtils;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -10,33 +11,43 @@ import com.dianping.pigeon.registry.exception.RegistryException;
 import com.dianping.pigeon.remoting.ServiceFactory;
 import com.dianping.pigeon.remoting.provider.publish.ServicePublisher;
 
+import java.util.Map;
+
 public class ServiceInitializeListener implements ApplicationListener {
 
 	private static final Logger logger = LoggerLoader.getLogger(ServiceInitializeListener.class);
 
 	public void onApplicationEvent(ApplicationEvent event) {
-		if (ServicePublisher.isAutoPublish()) {
-			if (event instanceof ContextRefreshedEvent) {
-				ContextRefreshedEvent refreshEvent = (ContextRefreshedEvent) event;
-				if (refreshEvent.getApplicationContext().getParent() == null) {
-					Thread t = new Thread(new Runnable() {
-
-						@Override
-						public void run() {
-							logger.info("service initialized");
-							try {
-								ServiceFactory.online();
-							} catch (RegistryException e) {
-								logger.error("error with services online", e);
-							}
+		synchronized (ServiceInitializeListener.class) {
+			if (ServicePublisher.isAutoPublish() && !isOnline()) {
+				if (event instanceof ContextRefreshedEvent) {
+					ContextRefreshedEvent refreshEvent = (ContextRefreshedEvent) event;
+					if (refreshEvent.getApplicationContext().getParent() == null) {
+						logger.info("service initialized");
+						try {
+							ServiceFactory.online();
+						} catch (RegistryException e) {
+							logger.error("error with services online", e);
 						}
-
-					});
-					t.setDaemon(true);
-					t.setName("Pigeon-Service-Initialize-Listener");
-					t.start();
+					}
 				}
 			}
 		}
+	}
+
+	private static boolean isOnline() {
+		boolean isOnline = true;
+		Map<String, Integer> weights = ServicePublisher.getServerWeight();
+		if (!CollectionUtils.isEmpty(weights)) {
+			for (Integer weight : weights.values()) {
+				if (weight <= 0) {
+					isOnline = false;
+					break;
+				}
+			}
+		} else {
+			isOnline = false;
+		}
+		return isOnline;
 	}
 }
