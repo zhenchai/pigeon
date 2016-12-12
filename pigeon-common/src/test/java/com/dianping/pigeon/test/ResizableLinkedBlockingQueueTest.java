0eb826cd9d104e3e0afc62b22941c93f12d87851
ChenChongze <chenchongze@ChenChongzedeMacBook-Pro.local>
2016-12-12 16:47:57 +0800

    add test

--- pigeon-common/src/test/java/com/dianping/pigeon/test/ResizableLinkedBlockingQueueTest.java
+++ pigeon-common/src/test/java/com/dianping/pigeon/test/ResizableLinkedBlockingQueueTest.java
@@ -17,7 +17,8 @@ public class ResizableLinkedBlockingQueueTest {
     public void test() {
         final int round = 20;
         final long offerSpeed = 700;
-        final long pollSpeed = 700;
+        final long pollSpeed = 1400;
+        final long refreshSpeed = 350;
         final long resizeSpeed = 10000;
         final int resizeStep = 25;
         final int initQueueCapacity = 100;
@@ -33,7 +34,7 @@ public class ResizableLinkedBlockingQueueTest {
                 while (true) {
                     try {
                         System.out.println("size: " + queue.size() + ", capacity: " + queue.getCapacity());
-                        Thread.sleep(pollSpeed);
+                        Thread.sleep(refreshSpeed);
                     } catch (InterruptedException e) {
                         //
                     }
@@ -101,9 +102,8 @@ public class ResizableLinkedBlockingQueueTest {
     public void test1() {
         final int round = 20;
         final long offerSpeed = 700;
-        final long pollSpeed = 700;
-        final long resizeSpeed = 10000;
-        final int resizeStep = 25;
+        final long pollSpeed = 1400;
+        final long refreshSpeed = 350;
         final int initQueueCapacity = 100;
         ThreadPool jobPool = new DefaultThreadPool("job");
         ThreadPool offerPool = new DefaultThreadPool("offerPool", round, round);
@@ -117,7 +117,7 @@ public class ResizableLinkedBlockingQueueTest {
                 while (true) {
                     try {
                         System.out.println("size: " + queue.size() + ", capacity: " + queue.remainingCapacity());
-                        Thread.sleep(pollSpeed);
+                        Thread.sleep(refreshSpeed);
                     } catch (InterruptedException e) {
                         //
                     }
@@ -125,23 +125,72 @@ public class ResizableLinkedBlockingQueueTest {
             }
         });
 
-        /*jobPool.execute(new Runnable() {
+        for (int i = 0; i < 20; ++i) {
+
+            offerPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            queue.offer(new Object());
+                            Thread.sleep(offerSpeed);
+                        } catch (InterruptedException e) {
+                            //
+                        }
+                    }
+                }
+            });
+
+            pollPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            queue.poll();
+                            Thread.sleep(pollSpeed);
+                        } catch (InterruptedException e) {
+                            //
+                        }
+                    }
+                }
+            });
+        }
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test2() {
+        final int round = 3000;
+        final long offerSpeed = 1;
+        final long pollSpeed = 1;
+        final long refreshSpeed = 1;
+        final int initQueueCapacity = 100;
+        ThreadPool jobPool = new DefaultThreadPool("job");
+        ThreadPool offerPool = new DefaultThreadPool("offerPool", round, round);
+        ThreadPool pollPool = new DefaultThreadPool("pollPool", round, round);
+
+        final LinkedBlockingQueue<Object> queue = new LinkedBlockingQueue<>(initQueueCapacity);
+
+        jobPool.execute(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0, capacity = initQueueCapacity; ; ++i) {
+                while (true) {
                     try {
-                        Thread.sleep(resizeSpeed);
-                        capacity -= resizeStep;
-                        if (capacity <= 0) {
-                            capacity = initQueueCapacity;
+                        if (initQueueCapacity != (queue.size() + queue.remainingCapacity())) {
+                            System.out.println("oh no");
                         }
-                        queue.setCapacity(capacity);
+                        Thread.sleep(1);
                     } catch (InterruptedException e) {
                         //
                     }
                 }
             }
-        });*/
+        });
 
         for (int i = 0; i < 20; ++i) {
 
