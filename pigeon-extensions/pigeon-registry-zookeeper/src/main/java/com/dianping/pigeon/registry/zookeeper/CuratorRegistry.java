389ffea69855ce2f1d631b9c3ad97e13042de58b
ChenChongze <chenchongze@ChenChongzedeMacBook-Pro.local>
2016-12-19 10:10:55 +0800

    fix version blank

--- pigeon-extensions/pigeon-registry-zookeeper/src/main/java/com/dianping/pigeon/registry/zookeeper/CuratorRegistry.java
+++ pigeon-extensions/pigeon-registry-zookeeper/src/main/java/com/dianping/pigeon/registry/zookeeper/CuratorRegistry.java
@@ -452,7 +452,7 @@ public class CuratorRegistry implements Registry {
         String version = getServerVersion(serviceAddress);
 
         if (StringUtils.isBlank(version)) {
-            version = "";
+            throw new RegistryException("version is blank");
         }
 
         return VersionUtils.isThriftSupported(version);
