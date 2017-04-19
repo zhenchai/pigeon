0513622d8e0bbd6508d2dcb0d217d3a9e6594f13
qi.yin <qi.yin@dianping.com>
2017-04-19 18:28:04 +0800

    fix context value null

--- pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/common/util/ContextUtils.java
+++ pigeon-remoting/src/main/java/com/dianping/pigeon/remoting/common/util/ContextUtils.java
@@ -6,167 +6,171 @@ import java.util.Map;
 
 public class ContextUtils {
 
-	private static ThreadLocal<Map> localContext = new ThreadLocal<Map>();
+    private static ThreadLocal<Map> localContext = new ThreadLocal<Map>();
 
-	private static ThreadLocal<Map<String, Serializable>> globalContext = new ThreadLocal<Map<String, Serializable>>();
+    private static ThreadLocal<Map<String, Serializable>> globalContext = new ThreadLocal<Map<String, Serializable>>();
 
-	private static ThreadLocal<Map<String, Serializable>> requestContext = new ThreadLocal<Map<String, Serializable>>();
+    private static ThreadLocal<Map<String, Serializable>> requestContext = new ThreadLocal<Map<String, Serializable>>();
 
-	private static ThreadLocal<Map<String, Serializable>> responseContext = new ThreadLocal<Map<String, Serializable>>();
+    private static ThreadLocal<Map<String, Serializable>> responseContext = new ThreadLocal<Map<String, Serializable>>();
 
-	public static void putLocalContext(Object key, Object value) {
-		Map<Object, Object> context = localContext.get();
-		if (context == null) {
-			context = new HashMap<Object, Object>();
-			localContext.set(context);
-		}
-		context.put(key, value);
-	}
+    public static void putLocalContext(Object key, Object value) {
+        Map<Object, Object> context = localContext.get();
+        if (context == null) {
+            context = new HashMap<Object, Object>();
+            localContext.set(context);
+        }
+        context.put(key, value);
+    }
 
-	public static Map getLocalContext() {
-		return localContext.get();
-	}
+    public static Map getLocalContext() {
+        return localContext.get();
+    }
 
-	public static Object getLocalContext(Object key) {
-		Map context = localContext.get();
-		if (context == null) {
-			return null;
-		}
-		return context.get(key);
-	}
+    public static Object getLocalContext(Object key) {
+        Map context = localContext.get();
+        if (context == null) {
+            return null;
+        }
+        return context.get(key);
+    }
 
-	public static void clearLocalContext() {
-		Map context = localContext.get();
-		if (context != null) {
-			context.clear();
-		}
-		localContext.remove();
-	}
+    public static void clearLocalContext() {
+        Map context = localContext.get();
+        if (context != null) {
+            context.clear();
+        }
+        localContext.remove();
+    }
 
-	public static void putGlobalContext(String key, Serializable value) {
-		Map<String, Serializable> context = globalContext.get();
-		if (context == null) {
-			context = new HashMap<String, Serializable>();
-			globalContext.set(context);
-		}
-		context.put(key, value);
-	}
+    public static void putGlobalContext(String key, Serializable value) {
+        Map<String, Serializable> context = globalContext.get();
+        if (context == null) {
+            context = new HashMap<String, Serializable>();
+            globalContext.set(context);
+        }
+        context.put(key, value);
+    }
 
-	public static void setGlobalContext(Map<String, Serializable> context) {
-		globalContext.set(context);
-	}
+    public static void setGlobalContext(Map<String, Serializable> context) {
+        globalContext.set(context);
+    }
 
-	public static Map<String, Serializable> getGlobalContext() {
-		return globalContext.get();
-	}
+    public static Map<String, Serializable> getGlobalContext() {
+        return globalContext.get();
+    }
 
-	public static Serializable getGlobalContext(String key) {
-		Map<String, Serializable> context = globalContext.get();
-		if (context == null) {
-			return null;
-		}
-		return context.get(key);
-	}
+    public static Serializable getGlobalContext(String key) {
+        Map<String, Serializable> context = globalContext.get();
+        if (context == null) {
+            return null;
+        }
+        return context.get(key);
+    }
 
-	public static void clearGlobalContext() {
-		Map<String, Serializable> context = globalContext.get();
-		if (context != null) {
-			context.clear();
-		}
-		globalContext.remove();
-	}
+    public static void clearGlobalContext() {
+        Map<String, Serializable> context = globalContext.get();
+        if (context != null) {
+            context.clear();
+        }
+        globalContext.remove();
+    }
 
-	public static void initRequestContext() {
-		Map<String, Serializable> context = requestContext.get();
-		if (context == null) {
-			context = new HashMap<String, Serializable>();
-			requestContext.set(context);
-		}
-	}
+    public static void initRequestContext() {
+        Map<String, Serializable> context = requestContext.get();
+        if (context == null) {
+            context = new HashMap<String, Serializable>();
+            requestContext.set(context);
+        }
+    }
 
-	public static void putRequestContext(String key, Serializable value) {
-		Map<String, Serializable> context = requestContext.get();
-		if (context == null) {
-			context = new HashMap<String, Serializable>();
-			requestContext.set(context);
-		}
-		context.put(key, value);
-	}
+    public static void putRequestContext(String key, Serializable value) {
+        Map<String, Serializable> context = requestContext.get();
+        if (context == null) {
+            context = new HashMap<String, Serializable>();
+            requestContext.set(context);
+        }
+        context.put(key, value);
+    }
 
-	public static Map<String, Serializable> getRequestContext() {
-		return requestContext.get();
-	}
+    public static Map<String, Serializable> getRequestContext() {
+        return requestContext.get();
+    }
 
-	public static Serializable getRequestContext(String key) {
-		Map<String, Serializable> context = requestContext.get();
-		if (context == null) {
-			return null;
-		}
-		return context.get(key);
-	}
+    public static Serializable getRequestContext(String key) {
+        Map<String, Serializable> context = requestContext.get();
+        if (context == null) {
+            return null;
+        }
+        return context.get(key);
+    }
 
-	public static void clearRequestContext() {
-		Map<String, Serializable> context = requestContext.get();
-		if (context != null) {
-			context.clear();
-		}
-		requestContext.remove();
-	}
+    public static void clearRequestContext() {
+        Map<String, Serializable> context = requestContext.get();
+        if (context != null) {
+            context.clear();
+        }
+        requestContext.remove();
+    }
 
-	public static void putResponseContext(String key, Serializable value) {
-		Map<String, Serializable> context = responseContext.get();
-		if (context == null) {
-			context = new HashMap<String, Serializable>();
-			responseContext.set(context);
-		}
-		context.put(key, value);
-	}
+    public static void putResponseContext(String key, Serializable value) {
+        Map<String, Serializable> context = responseContext.get();
+        if (context == null) {
+            context = new HashMap<String, Serializable>();
+            responseContext.set(context);
+        }
+        context.put(key, value);
+    }
 
-	public static Map<String, Serializable> getResponseContext() {
-		return responseContext.get();
-	}
+    public static Map<String, Serializable> getResponseContext() {
+        return responseContext.get();
+    }
 
-	public static Serializable getResponseContext(String key) {
-		Map<String, Serializable> context = responseContext.get();
-		if (context == null) {
-			return null;
-		}
-		return context.get(key);
-	}
+    public static Serializable getResponseContext(String key) {
+        Map<String, Serializable> context = responseContext.get();
+        if (context == null) {
+            return null;
+        }
+        return context.get(key);
+    }
 
-	public static void setResponseContext(Map<String, Serializable> context) {
-		responseContext.set(context);
-	}
+    public static void setResponseContext(Map<String, Serializable> context) {
+        responseContext.set(context);
+    }
 
-	public static void clearResponseContext() {
-		Map<String, Serializable> context = responseContext.get();
-		if (context != null) {
-			context.clear();
-		}
-		responseContext.remove();
-	}
+    public static void clearResponseContext() {
+        Map<String, Serializable> context = responseContext.get();
+        if (context != null) {
+            context.clear();
+        }
+        responseContext.remove();
+    }
 
-	public static void convertContext(Map<String, Serializable> srcCtx, Map<String, String> dstCtx) {
-		if (srcCtx != null) {
-			for (Map.Entry<String, Serializable> entry : srcCtx.entrySet()) {
-				if (entry.getValue() instanceof String) {
-					dstCtx.put(entry.getKey(), (String) entry.getValue());
-				} else {
-					throw new IllegalArgumentException("only support string type.");
-				}
-			}
-		}
-	}
+    public static void convertContext(Map<String, Serializable> srcCtx, Map<String, String> dstCtx) {
+        if (srcCtx != null) {
+            for (Map.Entry<String, Serializable> entry : srcCtx.entrySet()) {
+                if (entry.getValue() == null) {
+                    dstCtx.put(entry.getKey(), null);
+                }
+                if (entry.getValue() instanceof String) {
+                    dstCtx.put(entry.getKey(), (String) entry.getValue());
+                } else {
+                    throw new IllegalArgumentException("only support string type.");
+                }
 
-	public static Map<String, Serializable> convertContext(Map<String, String> srcCtx) {
-		Map<String, Serializable> dstCtx = new HashMap<String, Serializable>();
+            }
+        }
+    }
 
-		if (srcCtx != null) {
-			for (Map.Entry<String, String> entry : srcCtx.entrySet()) {
-				dstCtx.put(entry.getKey(), entry.getValue());
-			}
-		}
+    public static Map<String, Serializable> convertContext(Map<String, String> srcCtx) {
+        Map<String, Serializable> dstCtx = new HashMap<String, Serializable>();
 
-		return dstCtx;
-	}
+        if (srcCtx != null) {
+            for (Map.Entry<String, String> entry : srcCtx.entrySet()) {
+                dstCtx.put(entry.getKey(), entry.getValue());
+            }
+        }
+
+        return dstCtx;
+    }
 }
