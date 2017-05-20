7dd3a27aee1c67a7115bdd7c6c3eb72fa2c46b5b
ChenChongze <chenchongze@ChenChongzedeMacBook-Pro.local>
2017-05-20 16:29:32 +0800

    2.9.10-SNAPSHOT init

--- pigeon-common/src/main/java/com/dianping/pigeon/util/VersionUtils.java
+++ pigeon-common/src/main/java/com/dianping/pigeon/util/VersionUtils.java
@@ -2,7 +2,7 @@ package com.dianping.pigeon.util;
 
 public class VersionUtils {
 
-	public static final String VERSION = "2.9.9";
+	public static final String VERSION = "2.9.10-SNAPSHOT";
 	
     public static final String DP_THRIFT_VERSION = "2.8.0";
 
